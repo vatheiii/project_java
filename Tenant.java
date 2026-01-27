ee4ff5bae7c2e4cf18eded98d2230ff635326253
Vicheka <andronikaalison@gmail.com>
2026-01-27 14:47:39 +0700

    added more items

--- Tenant.java
+++ Tenant.java
@@ -1,5 +1,6 @@
 public class Tenant {
     String name;
+    String age;
     String phoneNumber;
     String email;
     int roomNumber;
@@ -7,14 +8,46 @@ public class Tenant {
     boolean hasPets;
     int floor;
     
-    public Tenant(String name, String phoneNumber, String email, int roomNumber, boolean hasActiveContract, boolean hasPets, int floor) {
+    public Tenant(String name, String phoneNumber, String email, int roomNumber, boolean hasActiveContract, boolean hasPets, int floor, String age) {
         this.name = name;
+        this.age = age;
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.roomNumber = roomNumber;
         this.hasActiveContract = hasActiveContract;
-        this.floor = floor;
         this.hasPets = hasPets;
+        this.floor = floor;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public boolean hasActiveContract() {
+        return hasActiveContract;
+    }
+
+    public boolean hasPets() {
+        return hasPets;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
 }
